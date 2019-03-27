de53e0fbe3e9f29ba23f8ab84cf374172c013406
KobayashiKz <anuuihsa9711212@gmail.com>
2019-03-28 00:09:17 +0900

    トップページ作成

---
+++ lib/content_widget.dart
@@ -0,0 +1,74 @@
+
+import 'package:flutter/material.dart';
+
+class ContentWidget extends StatelessWidget {
+  final String imageName;
+  final String title;
+  final String userName;
+
+  ContentWidget({Key key, @required this.imageName, @required this.title,
+    @required this.userName}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        _contentImageView(),
+        _contentText(),
+      ],
+    );
+  }
+
+  Widget _contentImageView() {
+    return Container(
+      height: 380,
+      width: 254,
+      child: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 5.0),
+              child: Image.asset(imageName, fit: BoxFit.fill,),
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            margin: EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0, bottom: 30.0),
+            child: Text(title,
+              style: TextStyle(color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _contentText() {
+    return Container(
+      height: 35,
+      width: 254,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            width: 30,
+            height: 30,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(image: AssetImage(imageName),),
+            ),
+          ),
+          Text(userName,
+            style: TextStyle(fontSize: 18.0,
+              fontStyle: FontStyle.italic
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,214 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_challenge/content_widget.dart';
+import 'package:flutter_ui_challenge/dialogonal_clipper.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Stack(
+        children: <Widget>[
+          _buildBackground(),
+          _buildMainContents(),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      bottomNavigationBar: _buildBottomNavigation(),
+    );
+  }
+
+  Widget _buildMainContents() {
+    return Column(
+      children: <Widget>[
+        _buildHeaderMenu(),
+        SizedBox(
+          height: 440.0,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+            _buildTopText(),
+            _buildTopImageList(),
+            ],
+          ),
         ),
+      ],
+    );
+  }
+
+  Widget _buildTopImageList() {
+    return
+      Row(
+      children: <Widget>[
+        ContentWidget(imageName: "assets/dark.jpg", title: "dark", userName: "user"),
+        ContentWidget(imageName: "assets/mountain.jpg", title: "mountain", userName: "user",),
+        ContentWidget(imageName: "assets/night.jpg", title: "night", userName: "user"),
+        ContentWidget(imageName: "assets/sea.jpg", title: "sea", userName: "user"),
+        ContentWidget(imageName: "assets/sun.jpg", title: "sun", userName: "user"),
+      ],
+    );
+  }
+
+  Widget _buildHeaderMenu() {
+    return Container(
+      margin: EdgeInsets.only(top: 40.0),
+      height: 50.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Text("Popular",
+                style: TextStyle(fontSize: 21.0,
+                    fontStyle: FontStyle.italic),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.center,
+              child: Text("Latest",
+                style: TextStyle(fontSize: 21.0,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black54),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text("Futured",
+                style: TextStyle(fontSize: 21.0,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black54),
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildTopText() {
+    return Container(
+      margin: EdgeInsets.only(left: 40.0, top: 40.0, bottom: 40.0),
+      height: 350,
+      width: 150,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _buildTitleText(),
+          _buildCategoryText("See all"),
+          _buildLine(),
+          _buildCategoryText("news"),
+          _buildCategoryText("sports"),
+          _buildCategoryText("weather"),
+          _buildCategoryText("radio"),
+          _buildCategoryText("music"),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTitleText() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5.0),
+      child: Text("MOST\nPOPULAR",
+        style: TextStyle(fontSize: 32.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.black54),
+      ),
+    );
+  }
+
+  Widget _buildCategoryText(String title) {
+    return Container(
+      child: Text(title,
+        style: TextStyle(fontSize: 22.0,
+        color: Colors.black54),
+      ),
+      margin: EdgeInsets.symmetric(vertical: 4.0),
+    );
+  }
+
+  Widget _buildLine() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 15.0),
+      height: 3.0,
+      width: 80.0,
+      color: Colors.grey[300],
+    );
+  }
+
+  Widget _buildBackground() {
+    return ClipPath (
+          clipper: DialogonalClipper(),
+          child: Container(
+            color: Colors.grey,
+          ),
+    );
+  }
+
+  Widget _buildBottomNavigation() {
+    return BottomNavigationBar(
+      iconSize: 26.0,
+      currentIndex: 2,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home,
+            color: Colors.black87,
+          ),
+          title: Text(""),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search,
+            color: Colors.black12,
+          ),
+          title: Text(""),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_circle,
+            color: Colors.black54,
+          ),
+          title: Text(""),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.content_copy,
+            color: Colors.black12,
+          ),
+          title: Text(""),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.people,
+            color: Colors.black12,
+          ),
+          title: Text(""),
+        ),
+      ],
     );
   }
 }
